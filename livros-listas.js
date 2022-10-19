5f5cea9778ef5f95a6d318e915cc6d16019e8ac6
GugeLucas <lucas.gugelmin@gmail.com>
2022-10-19 09:14:02 -0400

    Create livros-listas.js

---
+++ livros-listas.js
@@ -0,0 +1,27 @@
+//
+
+const precosLivros = [
+    {
+        titulo: "JV Script",
+        preco: 25
+    },
+    { 
+        titulo: "PHP",
+        preco:15
+    
+    },
+    {   titulo:"Java",
+        preco: 30
+    }, 
+    {   titulo:"Elixir",
+        preco: 50
+    },
+    {  titulo:"Go",
+        preco:45
+    }, 
+    {   titulo: "python",
+        preco: 20
+    }
+];
+
+module.exports = precosLivros;
